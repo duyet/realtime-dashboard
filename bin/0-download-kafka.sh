3f0db2b541b4d36a4d3a2b5f97eb6a94710077a5
Duyet Le <lvduit08@gmail.com>
2017-11-16 13:24:12 +0700

    Add bin/

---
+++ bin/0-download-kafka.sh
@@ -0,0 +1,3 @@
+wget http://mirrors.viethosting.com/apache/kafka/1.0.0/kafka_2.11-1.0.0.tgz
+tar -xzf kafka_2.11-1.0.0.tgz
+cd kafka_2.11-1.0.0
---
+++ bin/1-start-kafka.sh
@@ -0,0 +1,5 @@
+# Start zookeeper
+bin/zookeeper-server-start.sh config/zookeeper.properties
+
+# Start Kafka
+bin/kafka-server-start.sh config/server.properties
